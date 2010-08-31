f55193967e403efa2f25cc5d86234472c0f527ef
herewithme <amaury@beapi.fr>
2010-08-31 12:24:55 +0000

    release 1.0.9

---
+++ inc/functions.inc.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This method allow to link a custom post type by passing his ID, the second param allow to put one or lot's of objects ID...
+ *
+ * @param integer $custom_id 
+ * @param array $object_ids 
+ * @param array $post_types 
+ * @param boolean $append (Optionnal)
+ * @return void
+ * @author Amaury Balmer
+ */
+function rpt_set_object_relation( $custom_id = 0, $object_ids = array(), $post_types = array(), $append = true ) {
+	global $wpdb;
+	
+	// Object ID is valid ?
+	$custom_id = (int) $custom_id;
+	if ( $custom_id == 0 ) {
+		return false;
+	}
+	
+	// No append ? replace ! delete before !
+	if ( $append == false ) {
+		rpt_delete_object_relation( $custom_id, $post_types );
+	}
+	
+	// Always an array ?
+	if ( !is_array($object_ids) )
+		$object_ids = array( (int) $object_ids );
+		
+	// Cast values and make unique
+	$object_ids = array_map( 'intval', $object_ids );
+	$object_ids = array_unique( $object_ids );
+	
+	// No valid ID ?
+	if ( empty($object_ids) )
+		return false;
+		
+	// Loop for insert on DB !
+	foreach( (array) $object_ids as $object_id ) {
+		if ( $object_id == 0 || $object_id == $custom_id ) continue; // No zero, no master/master !
+		
+		$wpdb->insert( $wpdb->posts_relations, array( 'object_id_1' => $custom_id, 'object_id_2' => $object_id ) );
+	}
+	
+	return true;
+}
+
+/**
+ * This function allow to get for a specific post ID, all object ID of one or many post type
+ *
+ * @param integer $custom_id 
+ * @param array $post_types
+ * @return void
+ * @author Amaury Balmer
+ */
+function rpt_get_object_relation( $custom_id = 0, $post_types = array() ) {
+	global $wpdb;
+	
+	// Object ID is valid ?
+	$custom_id = (int) $custom_id;
+	if ( $custom_id == 0 ) {
+		return false;
+	}
+	
+	// Always an array for post type ?
+	if ( is_string($post_types) && !empty($post_types) )
+		$post_types = array( $post_types );
+	
+	$restrict_posts = '';
+	if ( !empty($post_types) ) {
+		$ids = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE ID != %d AND post_type IN ('".implode("', '",$post_types)."')", $custom_id));
+		if ( $ids == false )
+			return false;
+		$restrict_posts = " AND (object_id_1 IN (".implode(',',$ids).") OR object_id_2 IN (".implode(',',$ids)."))";
+	}
+		
+	// Make query to get relation ID depending the post types !
+ 	$results = $wpdb->get_results($wpdb->prepare("SELECT * FROM $wpdb->posts_relations WHERE (object_id_1 = %d OR object_id_2 = %d) $restrict_posts", $custom_id, $custom_id));
+ 	
+	// Clean array for return... only take the right ID...
+	$post_ids = array();
+	foreach( (array) $results as $result ) {
+		if ( $result->object_id_1 == $custom_id ) {
+			$post_ids[] = $result->object_id_2;
+		} else { // object_id_2
+			$post_ids[] = $result->object_id_1;
+		}
+	}
+	
+	return $post_ids;
+}
+
+/**
+ * This method allow to delete relations
+ *
+ * @param string $custom_id
+ * @param array $post_types
+ * @return void
+ * @author Amaury Balmer
+ */
+function rpt_delete_object_relation( $custom_id = 0, $post_types = array() ) {
+	global $wpdb;
+	
+	$custom_id = (int) $custom_id;
+	if ( $custom_id == 0 ) 
+		return false;
+		
+	// Always an array for post type ?
+	if ( is_string($post_types) && !empty($post_types) )
+		$post_types = array( $post_types );
+	elseif ( empty($post_types) ) {
+		$post_types = array( get_post_type($custom_id) );
+	}
+		
+	$restrict_posts = '';
+	if ( !empty($post_types) ) {
+		$ids = $wpdb->get_col("SELECT ID FROM $wpdb->posts WHERE post_type IN ('".implode("', '",$post_types)."')");
+		if ( $ids == false )
+			return false;
+		$restrict_posts = " AND (object_id_1 IN (".implode(',',$ids).") OR object_id_2 IN (".implode(',',$ids)."))";
+	}
+
+	return $wpdb->query( $wpdb->prepare("DELETE FROM $wpdb->posts_relations WHERE (object_id_1 = %d OR object_id_2 = %d) $restrict_posts", $custom_id, $custom_id) );
+}
+?>
---
+++ relations-post-types.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: Relation Post Types
+Plugin URI: http://redmine.beapi.fr/projects/show/relations-post-types
+Description: Allow to build relations between 2 custom types.
+Author: Amaury Balmer
+Author URI: http://www.beapi.fr
+Version: 1.0.9
+Text Domain: relations-post-types
+Domain Path: /languages/
+Network: false
+
+----
+
+Copyright 2010 Amaury Balmer (amaury@beapi.fr)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+---
+
+Todo :
+	Core
+
+	Admin
+	
+	Extras
+	
+	Client
+		A simple function for template for display related content !
+*/
+
+// Setup table name for relations
+global $wpdb;
+$wpdb->tables[] 		= 'posts_relations';
+$wpdb->posts_relations 	= $wpdb->prefix . 'posts_relations';
+
+// Folder name
+define ( 'RPT_VERSION', '1.0.9' );
+define ( 'RPT_OPTION',  'relations-post-types' );
+
+define ( 'RPT_URL', plugins_url('', __FILE__) );
+define ( 'RPT_DIR', dirname(__FILE__) );
+
+// Library
+require( RPT_DIR . '/inc/functions.inc.php' );
+require( RPT_DIR . '/inc/functions.tpl.php' );
+
+// Call client class and functions
+require( RPT_DIR . '/inc/class.base.php' );
+require( RPT_DIR . '/inc/class.walker.php' );
+require( RPT_DIR . '/inc/class.client.php' );
+require( RPT_DIR . '/inc/class.widget.php' );
+
+if ( is_admin() ) { // Call admin class
+	require( RPT_DIR . '/inc/class.admin.php' );
+	require( RPT_DIR . '/inc/class.admin.post.php' );
+}
+
+// Activate/Desactive Relation Post Types
+register_activation_hook  ( __FILE__, array('RelationsPostTypes_Base', 'activate') );
+register_deactivation_hook( __FILE__, array('RelationsPostTypes_Base', 'deactivate') );
+
+add_action( 'plugins_loaded', 'initRelationsPostTypes' );
+function initRelationsPostTypes() {
+	global $relations_post_types;
+	
+	// Load translations
+	load_plugin_textdomain ( 'relations-post-types', false, RPT_FOLDER . 'languages' );
+	
+	// Client
+	$relations_post_types['client-base']  = new RelationsPostTypes_Client();
+	
+	// Admin
+	if ( is_admin() ) {
+		$relations_post_types['admin-base'] = new RelationsPostTypes_Admin();
+		$relations_post_types['admin-post'] = new RelationsPostTypes_Admin_Post();
+	}
+	
+	// Widget
+	add_action( 'widgets_init', create_function('', 'return register_widget("RelationsPostTypes_Widget");') );
+}
+?>
